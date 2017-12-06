de2a20737ec9a70ca2dea4199b67ccf1d0cd8ae5
alex_r <blank.basic@gmail.com>
2017-12-06 23:21:10 +0300

    update

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,6 +1,6 @@
 def to_rna(dna: str) -> str:
     TRANSLATION_TABLE = str.maketrans("GCTA", "CGAU")
     if not all(ord(x) in TRANSLATION_TABLE for x in dna):
-        return ''
+        raise ValueError
 
     return dna.translate(TRANSLATION_TABLE)
