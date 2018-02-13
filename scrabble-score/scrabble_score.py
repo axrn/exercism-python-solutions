8d113ae39af943c5c9e28f5fa90804d7fcc0cdce
alex_r <blank.basic@gmail.com>
2018-02-13 11:06:22 +0300

    next iteration

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,9 +1,15 @@
 def score(word: str) -> int:
-    score = ['', 'AEIOULNRST', 'DG', 'BCMP', 'FHVWY', 'K', '', '', 'JX', '', 'QZ']
-    res = []
-    for c in word.upper():
-        for i, e in enumerate(score):
-            if c in e:
-                res.append(i)
+    SCORE = {'AEIOULNRST': 1,
+             'DG': 2,
+             'BCMP': 3,
+             'FHVWY': 4,
+             'K': 5,
+             'JX': 8,
+             'QZ': 10}
+    res = 0
+    for char in word.upper():
+        for s in SCORE.keys():
+            if char in s:
+                res += SCORE[s]
                 continue
-    return sum(res)
+    return res
