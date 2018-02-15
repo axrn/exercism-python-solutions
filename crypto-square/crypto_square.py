2e55cc6a1a332fe97c8db0f7259006f680beefc2
alex_r <blank.basic@gmail.com>
2018-02-15 16:13:33 +0300

    next iteration

--- crypto-square/crypto_square.py
+++ crypto-square/crypto_square.py
@@ -1,11 +1,11 @@
-def encode(plain_text: str) -> str:
-    normalized = ''.join(x for x in plain_text.lower() if x.isalnum())
+def encode(text: str) -> str:
+    normalized = ''.join(x for x in text.lower() if x.isalnum())
     if len(normalized) <= 1:
         return normalized
 
     r = round(len(normalized) ** 0.5)
-    c = r if r * r >= len(normalized) else r + 1
+    c = round(len(normalized) / r)
 
-    filled = normalized + ' ' * (r * c - len(normalized))
+    filled = normalized + (r * c - len(normalized)) * ' '
 
     return ' '.join(filled[start::c] for start in range(c))
