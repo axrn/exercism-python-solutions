66264560000b6b85d778b5ab7347fd86e447b265
alex_r <blank.basic@gmail.com>
2018-02-16 22:29:13 +0300

    next iteration

--- sublist/sublist.py
+++ sublist/sublist.py
@@ -3,23 +3,16 @@ SUBLIST, SUPERLIST, EQUAL, UNEQUAL = range(4)
 
 def check_lists(first_list, second_list):
 
-    def is_sub(l1: list, l2: list) -> bool:
-        if len(l2) < len(l1):
-            return False
-        for start in range(0, len(l2) - len(l1) + 1):
-            if l2[start] != l1[0]:
-                continue
-            if l2[start: start + len(l1)] == l1:
-                return True
-        return False
+    first = str(first_list)[1:-1]
+    second = str(second_list)[1:-1]
 
-    if first_list == second_list:
+    if first == second:
         return EQUAL
 
-    if first_list == [] or is_sub(first_list, second_list):
+    if first in second:
         return SUBLIST
 
-    if second_list == [] or is_sub(second_list, first_list):
+    if second in first:
         return SUPERLIST
 
     return UNEQUAL
