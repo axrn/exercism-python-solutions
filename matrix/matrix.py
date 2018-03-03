b38b3ad754e90381fdb4d82ee2402aaec38d17c3
alex_r <blank.basic@gmail.com>
2018-03-03 22:44:43 +0300

    immutability

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -4,7 +4,7 @@ class Matrix(object):
         self._columns = [list(x) for x in zip(*self._rows)]
 
     def row(self, index: int) -> list:
-        return self._rows[index]
+        return self._rows[index][:]
 
     def column(self, index: int) -> list:
-        return self._columns[index]
+        return self._columns[index][:]
