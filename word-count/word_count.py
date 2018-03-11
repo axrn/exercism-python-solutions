fe6f3e5f6188d7f112ddce048033341f5af5708f
alex_r <blank.basic@gmail.com>
2018-03-11 18:14:26 +0300

    next iteration

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,9 +1,6 @@
+from collections import Counter
+import re
+
+
 def word_count(phrase):
-    import re
-    from collections import defaultdict
-
-    dd = defaultdict(int)
-    for word in filter(None, re.split("[\W_]", phrase)):
-        dd[word] += 1
-
-    return dict(dd)
+    return Counter(filter(None, re.split("[\W_]", phrase.lower())))
